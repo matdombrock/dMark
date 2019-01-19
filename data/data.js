4961f47dfedc509bbae10473e167555d6378cc66
Mathieu Dombrock <matdombrock@gmail.com>
2019-01-19 07:34:59 -0800

    first

---
+++ data/data.js
@@ -0,0 +1,40 @@
+//this could actually be an async function that returns from another file...
+//possible raw data on github
+var bookmarks_list = [
+  {
+    url:"https://example.com",
+    name:"Example",
+    desc:"Just an example",
+    tags: ["tag","test-tag","example-tag"]
+  },  
+  {
+    url:"https://mzero.space",
+    name:"mzero.space",
+    desc:"Mathieu Dombrock's Site",
+    tags: ["developer","portfolio","articles"]
+  },   
+  {
+    url:"https://bootswatch.com/",
+    name:"Bootswatch",
+    desc:"Bootstrap themes (pallets)",
+    tags: ["theme","bootstrap"]
+  },  
+  {
+    url:"https://mailgun.com",
+    name:"Mailgun",
+    desc:"The Email Service For Developers",
+    tags: ["email","api","tool"]
+  }, 
+  {
+    url:"https://stackoverflow.com/questions/38323880/error-eacces-permission-denied",
+    name:"Non-root global npm",
+    desc:"Fix npm not allowing for global installs",
+    tags: ["npm","fix","node"]
+  }, 
+  {
+    url:"https://stackoverflow.com/questions/38323880/error-eacces-permission-denied",
+    name:"Node separate routes",
+    desc:"How to separate routes on Node.js and Express 4?",
+    tags: ["node"]
+  },
+]
---
+++ js/custom.js
@@ -0,0 +1,68 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        app_name: "mMark",
+        s_tags: true,
+        s_desc: true,
+        s_name: true,
+        count:0,
+        search_query: "",
+        bookmarks: bookmarks_list,
+        results: [],
+    },
+    methods:{
+      search_tag: function(tag){
+        this.search_query = tag;
+        this.s_tags = true;
+        this.s_desc = false;
+        this.s_name = false;
+        this.search();
+      },
+      reset: function(){
+        this.search_query = "";
+        this.s_tags = true;
+        this.s_desc = true;
+        this.s_name = true;
+        this.search();
+      },
+      count_results: function(){
+        //console.log("count"+ $(".card").length);
+        //this.count =$(".card").length;
+      },
+      search: function(){
+        console.log("searching for "+this.search_query);
+        this.results = [];
+        this.count = 0;
+        var self = this;
+        this.bookmarks.forEach(function(bookmark){
+          console.log(bookmark.name);
+          var stars = 0;
+          if(self.s_tags == true){
+            if(bookmark.tags.includes(self.search_query)){
+              stars+=1;
+            }
+          }
+          if(self.s_name == true){
+            console.log("checking name "+bookmark.name+ "against "+self.search_query);
+            if(bookmark.name.includes(self.search_query)){
+              stars+=1;
+            }
+          }
+          if(self.s_desc){
+            if(bookmark.desc.includes(self.search_query)){
+              stars+=1;
+            }
+          }
+          if(stars!=0){
+            console.log("Match!");
+            self.count += 1;
+            bookmark.stars = stars;
+            self.results.push(bookmark);
+          }else{
+            console.log("No Match!");
+          }
+        });
+      }
+    }
+  })
+  app.search();
